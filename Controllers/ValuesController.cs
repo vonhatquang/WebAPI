a1a70f1468fa0eabb02f7ff47197bf7e2e863b91
Võ Nhật Quang <nhatquang0982@gmail.com>
2018-04-19 14:24:38 +0700

    Commit origin code

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BusinessManagementWebApi.Controllers
+namespace WebApi.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
--- Startup.cs
+++ Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace BusinessManagementWebApi
+namespace WebApi
 {
     public class Startup
     {
